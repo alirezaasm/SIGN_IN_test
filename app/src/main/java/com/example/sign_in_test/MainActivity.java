f28d00b5485fbdf50c24dc1cd408dd31d536ed27
MASON <aliasmdev@gmail.com>
2020-05-23 21:44:00 +0430

    Test

--- app/src/main/java/com/example/sign_in_test/MainActivity.java
+++ app/src/main/java/com/example/sign_in_test/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -19,53 +21,62 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     String url="https://tabeshma.000webhostapp.com/mysites/showparams.php";
    TextView tv;
+   EditText pass, user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         tv=findViewById(R.id.tv);
-
-        OkHttpClient client=new OkHttpClient();
-
-        RequestBody formBody = new FormBody.Builder()
-                .add("username", "111")
-                .add("password","555")
-                .build();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .post(formBody)
-                .build();
-        client.newCall(request).enqueue(new Callback() {
+        user=findViewById(R.id.user);
+        pass=findViewById(R.id.pass);
+        findViewById(R.id.sign_in).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFailure(Call call, IOException e) {
-                Log.i("failed in sign_in",e.getMessage());
-            }
+            public void onClick(View v) {
+                OkHttpClient client=new OkHttpClient();
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException
-            {
-                if(response.isSuccessful())
-                {
-                    final String myresponse=response.body().string();
-                    MainActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                tv.setText(myresponse);
+                RequestBody formBody = new FormBody.Builder()
+                        .add("username", user.getText().toString().trim())
+                        .add("password",pass.getText().toString().trim())
+                        .build();
 
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(formBody)
+                        .build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Log.i("failed in sign_in",e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException
+                    {
+                        if(response.isSuccessful())
+                        {
+                            final String myresponse=response.body().string();
+                            MainActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        tv.setText(myresponse);
+
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
 
 
+                                }
+                            });
                         }
-                    });
-                }
 
+                    }
+                });
             }
         });
+
+
 
 
     }
